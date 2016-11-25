ae8a771bced184c4dd94fa1d02218f96a5d81508
vmendi <vmendi@gmail.com>
2016-11-25 15:42:07 -0800

    Corner detection

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 # http://docs.opencv.org/trunk/d6/d00/tutorial_py_root.html
 
 
-def extract_max_contour_rect(img):
+def extract_max_contour_rect(img_idx, img):
     threshold_img = cv2.adaptiveThreshold(img, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C,
                                           thresholdType=cv2.THRESH_BINARY_INV, blockSize=5, C=3)
 
@@ -52,21 +52,65 @@ def extract_max_contour_rect(img):
 
     return rect
 
+
+def find_corners(img_idx, img, gray):
+    img = img.copy()
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+
+    # Result is dilated for marking the corners, not important
+    dst = cv2.dilate(dst, None)
+
+    # Threshold for an optimal value, it may vary depending on the image.
+    img[dst > 0.01*dst.max()]= [0, 0, 255]
+
+    cv2.imwrite('out/{0}-corners.png'.format(img_idx), img)
+
+
+def find_corners_subpixel(img_idx, img, gray):
+    img = img.copy()
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray, blockSize=10, ksize=10, k=0.04)
+    dst = cv2.dilate(dst, None)
+    ret, dst = cv2.threshold(dst, 0.01*dst.max(), 255, 0)
+    dst = np.uint8(dst)
+
+    # find centroids
+    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+
+    # define the criteria to stop and refine the corners
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+    corners = cv2.cornerSubPix(gray, np.float32(centroids), (5,5), (-1,-1), criteria)
+
+    # Now draw them
+    res = np.hstack((centroids, corners))
+    res = np.int8(res)
+    img[res[:, 1],res[:, 0]] = [0, 0, 255]
+    img[res[:, 3],res[:, 2]] = [0, 255, 0]
+
+    cv2.imwrite('out/{0}-corners.png'.format(img_idx), img)
+
+
+def iterate_over_images(img_names):
+    for img_idx, img_name in enumerate(img_names):
+        img = cv2.imread(img_name)
+        img = cv2.resize(img, (1024, 768))
+
+        height, width, color_depth = img.shape
+        img = img[50:height-50, 150:width-150]  # Crop from x, y, w, h -> 100, 200, 300, 400
+        # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        extract_max_contour_rect(img_idx, gray)
+        find_corners(img_idx, img, gray)
+
 if __name__ == '__main__':
     cv2.namedWindow('main', flags=cv2.WINDOW_NORMAL)
 
-    img_names = ['in/IMG_00064.jpg', 'in/IMG_00065.jpg', 'in/IMG_00066.jpg', 'in/IMG_00068.jpg', 'in/IMG_00070.jpg',
+    filenames = ['in/IMG_00064.jpg', 'in/IMG_00065.jpg', 'in/IMG_00066.jpg', 'in/IMG_00068.jpg', 'in/IMG_00070.jpg',
                  'in/IMG_00072.jpg', 'in/IMG_00073.jpg', 'in/IMG_00074.jpg', 'in/IMG_00075.jpg', 'in/IMG_00076.jpg',
                  'in/IMG_00077.jpg', 'in/IMG_00078.jpg']
 
-    for img_idx, img_name in enumerate(img_names):
-        img = cv2.imread(img_name, 0)   # 0 loads as grayscale
-        img = cv2.resize(img, (1024, 768))
-        height, width = img.shape
-        img = img[50:height-50, 150:width-150]  # Crop from x, y, w, h -> 100, 200, 300, 400
-                                                # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
-
-        extract_max_contour_rect(img)
-
+    iterate_over_images(filenames)
 
 cv2.destroyAllWindows()
