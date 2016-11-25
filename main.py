ebbfdc25bd24894b12dd57f2c74b3e7f3fa3752b
vmendi <vmendi@gmail.com>
2016-11-25 14:48:02 -0800

    Let there be light

---
+++ main.py
@@ -0,0 +1,72 @@
+import cv2
+import numpy as np
+
+# http://docs.opencv.org/trunk/d6/d00/tutorial_py_root.html
+
+
+def extract_max_contour_rect(img):
+    threshold_img = cv2.adaptiveThreshold(img, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C,
+                                          thresholdType=cv2.THRESH_BINARY_INV, blockSize=5, C=3)
+
+    # Gausian removes small edges during Canny
+    threshold_img = cv2.GaussianBlur(threshold_img, (11, 11), 0)
+    # threshold_img = cv2.bilateralFilter(threshold_img.copy(), dst=threshold_img, d=3, sigmaColor=250, sigmaSpace=250)
+    # threshold_img = cv2.erode(threshold_img.copy(), kernel=np.ones((1,1), np.uint8), iterations=2)
+    # threshold_img = cv2.morphologyEx(threshold_img.copy(), cv2.MORPH_OPEN, np.ones((5,5), np.uint8))
+
+    edges_img = cv2.Canny(threshold_img, 100, 255)
+
+    # Close before findContours helps with discontinuities in the perimeters
+    edges_img = cv2.morphologyEx(edges_img, cv2.MORPH_CLOSE, np.ones((5,5), np.uint8))
+
+    contours_img, contours, hierarchy = cv2.findContours(edges_img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    areas = [cv2.contourArea(contour) for contour in contours]
+    max_area_index = np.argmax(areas)
+    perimeter_of_max_area = cv2.arcLength(contours[max_area_index], True)
+
+    perimeters = [cv2.arcLength(contour, True) for contour in contours]
+    max_perimeter_index = np.argmax(perimeters)
+    area_of_max_perimeter = cv2.contourArea(contours[max_perimeter_index])
+
+    contours_img = np.zeros(threshold_img.shape)
+
+    levels = []
+    level_colors = [(255-idx*10, 255-idx*10, 255-idx*10) for idx in range(0, 25)]
+    curr_level = 0
+    while curr_level < len(level_colors):
+        levels.append([contour for idx, contour in enumerate(contours) if hierarchy[0][idx][3] == curr_level-1])
+        curr_level += 1
+    for level_idx, level in enumerate(levels):
+        cv2.drawContours(contours_img, contours=levels[level_idx], contourIdx= -1, color=level_colors[level_idx], thickness=1)
+
+    rect = cv2.minAreaRect(contours[max_area_index])
+    box = cv2.boxPoints(rect)
+    cv2.drawContours(contours_img, [np.array(box).astype(int)], 0, (200, 200, 200), thickness=2)
+
+    cv2.imwrite('out/{0}-threshold.png'.format(img_idx), threshold_img)
+    cv2.imwrite('out/{0}-edges.png'.format(img_idx), edges_img)
+    cv2.imwrite('out/{0}-contours.png'.format(img_idx), contours_img)
+
+    print("Image {0} bounding rect ({1:.0f},{2:.0f}), angle {3:.0f}".format(img_idx, rect[1][0], rect[1][1], rect[2]))
+
+    return rect
+
+if __name__ == '__main__':
+    cv2.namedWindow('main', flags=cv2.WINDOW_NORMAL)
+
+    img_names = ['in/IMG_00064.jpg', 'in/IMG_00065.jpg', 'in/IMG_00066.jpg', 'in/IMG_00068.jpg', 'in/IMG_00070.jpg',
+                 'in/IMG_00072.jpg', 'in/IMG_00073.jpg', 'in/IMG_00074.jpg', 'in/IMG_00075.jpg', 'in/IMG_00076.jpg',
+                 'in/IMG_00077.jpg', 'in/IMG_00078.jpg']
+
+    for img_idx, img_name in enumerate(img_names):
+        img = cv2.imread(img_name, 0)   # 0 loads as grayscale
+        img = cv2.resize(img, (1024, 768))
+        height, width = img.shape
+        img = img[50:height-50, 150:width-150]  # Crop from x, y, w, h -> 100, 200, 300, 400
+                                                # NOTE: its img[y: y + h, x: x + w] and *not* img[x: x + w, y: y + h]
+
+        extract_max_contour_rect(img)
+
+
+cv2.destroyAllWindows()
